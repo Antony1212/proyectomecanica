6d20010c38e875b6e83049e9eccfd3459725d415
Antony1212 <1004420182@unajma.edu.pe>
2022-12-16 22:54:06 -0500

    avance pagina login

--- Vistas/Modulos/ingreso.php
+++ Vistas/Modulos/ingreso.php
@@ -1,11 +1,47 @@
-<br> <!-- Vistas/Modulos/ingreso.php -->
-<h1>INGRESAR</h1>
+<body class="fondo-body">
+	<div class="row">
+		
+    <div class="col s12">
+		<div class="col s4 m4">
+			<div class=".container">
+			<div class="card">
+				<div class="card-image">
+				<img src="images/sample-1.jpg">
+				<span class="card-title">Card Title</span>
+				</div>
+				<div class="card-content">
+				<p>I am a very simple card. I am good at containing small bits of information.
+				I am convenient because I require little markup to use effectively.</p>
+				</div>
+				<div class="card-action">
+				<a href="#">This is a link</a>
+				</div>
+			</div>
+			</div>
+		</div>
+		<div class="col s6 m6">
+			<div class="card">
+				<div class="card-image">
+				<img src="images/sample-1.jpg">
+				<span class="card-title">Card Title</span>
+				</div>
+				<div class="card-content">
+				<p>I am a very simple card. I am good at containing small bits of information.
+				I am convenient because I require little markup to use effectively.</p>
+				</div>
+				<div class="card-action">
+				<a href="#">This is a link</a>
+				</div>
+			</div>
+			
+		</div>
+	</div>  
+ </div>
+    
+</body>
 
-<form method="post" action="">
-	<input type="text" placeholder="Usuario" name="usuarioI" required>
-	<input type="password" placeholder="Contraseña" name="claveI" required>
-	<input type="submit" value="Ingresar">
-</form>
+
+
 
 <?php
 $ingreso = new AdminC();
--- Vistas/plantilla.php
+++ Vistas/plantilla.php
@@ -3,8 +3,13 @@
 <head>
 	<meta charset="utf-8">
 	<title>CRUD</title>
+	    <!-- Compiled and minified CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
-	<link rel="stylesheet" type="text/css" href="Vistas/css/estilos.css">
+		<!-- Compiled and minified JavaScript -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>		
+
+		<link rel="stylesheet" type="text/css" href="Vistas/css/estilos.css">
 
 </head>
 <body>
