8bd7fc69d047d2a500e72a5665ddb27112a2f313
Antony1212 <1004420182@unajma.edu.pe>
2022-12-19 09:21:38 -0500

    avance inicio

--- Vistas/Modulos/ingreso.php
+++ Vistas/Modulos/ingreso.php
@@ -1,15 +1,20 @@
 <body class="fondo-body">
 	<div class="row">
-		
+	
+	<div class="col s1">
+			
+			</div>
+
     <div class="col s12">
 
 		
 		<div class="row">
+
 		
-			<div class= "col s12 m12 l8 offset-12 loginDiv">
+			<div class= "col s12 m12 l4 offset-12 loginDiv">
 				<div class="row loginContainer">
 					
-					<div class="col s12 m5 l5 offset-l1 offset-m1">
+					<div class="col s10 m10 10 offset-l1 offset-m1">
 						<div class="loginTitle">
 							<h5>Inicio de Session sistema de ventas</h5>
 							<div class="col s12">
@@ -45,6 +50,7 @@
 		
 	 
  </div>
+
     
 </body>
 
