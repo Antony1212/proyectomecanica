dc220104405b039def86f4411aec53a652b3af97
Antony1212 <1004420182@unajma.edu.pe>
2023-01-10 20:01:56 -0500

    agregando css

---
+++ Vistas/Modulos/barra.php
@@ -0,0 +1,17 @@
+
+	<?php //if($rutasC->sesionIniciadaC()): ?>
+		
+	<?php //else: ?>
+		<nav>
+    <div class="nav-wrapper">
+    
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+      <a href="#!" data-target="sidenav-left" class="sidenav-trigger left"><i class="material-icons black-text">menu</i></a>
+    </div>
+  </nav>
+		
+ <?php //endif; ?>
--- Vistas/Modulos/registrar.php
+++ Vistas/Modulos/registrar.php
@@ -2,33 +2,56 @@
 $registrar = new EmpleadosC();
 $registrar->registrarEmpleadosC();
 ?>
-<body class="fondo-body">
 
- <div class="row">
 
-<div class="col s12 m4 l3"> 
-<?php
 
-	include 'menu.php';
+	
 
-?>
-</div>
 
-<div class="col s12 m8 l9"> <!-- Note that "m8 l9" was added -->
-<h1>REGISTRAR UN EMPLEADO</h1>
 
-<form method="post" action="">
-	<input type="text" placeholder="Nombre" name="nombreR" required>
-	<input type="text" placeholder="Apellido" name="apellidoR" required>
-	<input type="email" placeholder="Email" name="emailR" required>
-	<input type="text" placeholder="Puesto" name="puestoR" required>
-	<input type="text" placeholder="Salario" name="salarioR" required>
-	<input type="submit" value="Registrar">
-</form>
+<div class="row">
 
-</div>
+	<div class="col s12"> 
+		<?php
+		include 'barra.php';
+		?>
+	</div>
 
-</div>
+      <div class="col s12 m4 l4"> 
 
-</body>
+	  <?php
+
+		include 'menu.php';
+
+	  ?>
+	   
+
+      </div>
+
+	  
+
+      <div class="col s12 m8 l8">
+
+	  
+		<form method="post" action="">
+			<input type="text" placeholder="Nombre" name="nombreR" required>
+			<input type="text" placeholder="Apellido" name="apellidoR" required>
+			<input type="email" placeholder="Email" name="emailR" required>
+			<input type="text" placeholder="Puesto" name="puestoR" required>
+			<input type="text" placeholder="Salario" name="salarioR" required>
+			<input type="submit" value="Registrar">
+		</form>
+
+      </div>
+
+	  
+	  
+    </div>
+
+
+
+
+
+
+
 
